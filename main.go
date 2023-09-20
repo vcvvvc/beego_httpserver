9afa98cb4eaa1306d9564b0facf64e22c4498896
Vc <vcvckw@gmail.com>
2023-09-20 23:15:15 +0800

    :init:

--- main.go
+++ main.go
@@ -1,10 +1,26 @@
 package main
 
 import (
-	beego "github.com/beego/beego/v2/server/web"
-	_ "httpserver/routers"
+	"github.com/beego/beego/v2/server/web"
 )
 
+type UserController struct {
+	web.Controller
+}
+
+func (u *UserController) HelloWorld() {
+	u.Ctx.WriteString("hello, world")
+
+}
+
+func (u *UserController) Index() {
+	u.TplName = "index.html"
+	u.Render()
+}
+
 func main() {
-	beego.Run()
+	web.SetStaticPath("/", "blog")
+	//web.CtrlGet("/", (*UserController).Index)
+	web.CtrlGet("/helloworld", (*UserController).HelloWorld)
+	web.Run()
 }
