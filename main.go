e5a1b282531df2c3be59f398a0a39f46b57bd0d6
Vc <vcvckw@gmail.com>
2023-09-27 22:59:07 +0800

    init

--- main.go
+++ main.go
@@ -1,37 +1,14 @@
 package main
 
 import (
-	"github.com/beego/beego/v2/client/orm"
-	// don't forget this
-	_ "github.com/go-sql-driver/mysql"
+	"httpserver/models"
 )
 
-// User -
-type User struct {
-	ID       int    `orm:"column(id)"`
-	Name     string `orm:"column(username)"`
-	Password string `orm:"column(password)"`
-}
-
 func init() {
-	// need to register models in init
-	orm.RegisterModel(new(User))
-
-	// need to register default database
-	orm.RegisterDataBase("default", "mysql", "root:Aa1248800211@tcp(127.0.0.1:3306)/gotest?charset=utf8")
+	models.RegisterDB()
 }
 
 func main() {
-	// automatically build table
-	orm.RunSyncdb("default", false, true)
+	println("ssss")
 
-	// create orm object
-	o := orm.NewOrm()
-
-	// data
-	user := new(User)
-	user.Name = "mike"
-
-	// insert data
-	o.Insert(user)
 }
