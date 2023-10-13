a0a346f25b2057763a0a449201c9772bec09e5f1
Vc <vcvckw@gmail.com>
2023-10-13 22:45:24 +0800

    init

--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
-import (
-	"httpserver/models"
-)
+import "httpserver/models"
 
 func init() {
 	// need to register models in init
 	//models.InsertDB()
-	models.DeleteDB(1)
+	//models.DeleteDB(3)
+	//models.UpdatePWD(2, "tesxxxxtvvvvv")
+	models.SearchUser(5, "test_transaction")
 }
 
 func main() {
-	println("ssss")
+	println("")
 
 }
