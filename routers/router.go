c8dd62978598af9ff682faf120c2cfd5f5a612f4
Vc <vcvckw@gmail.com>
2023-10-16 23:52:14 +0800

    router

--- routers/router.go
+++ routers/router.go
@@ -8,16 +8,10 @@ import (
 func init() {
 	web.CtrlGet("/", (*controllers.RegisterController).RegisterPage)
 	web.CtrlPost("/register", (*controllers.RegisterController).Register)
-	web.CtrlGet("/loginPage", (*controllers.LoginController).loginPage)
-	web.CtrlPost("/login", (*controllers.LoginController).login)
+	web.CtrlGet("/login", (*controllers.LoginController).LoginPage)
+	web.CtrlPost("/loginreq", (*controllers.LoginController).Login)
 
 	//beego.Router("/home", &controllers.BlogController{}, "*:Home")
-	//beego.Router("/article", &controllers.BlogController{}, "*:Article")
-	//beego.Router("/detail", &controllers.BlogController{}, "*:Detail")
-	//beego.Router("/about", &controllers.BlogController{}, "*:About")
-	//beego.Router("/timeline", &controllers.BlogController{}, "*:Timeline")
-	//beego.Router("/resource", &controllers.BlogController{}, "*:Resource")
-	//beego.Router("/comment", &controllers.BlogController{}, "post:Comment")
 	//
 	//beego.AutoRouter(&controllers.AdminController{})
 }
